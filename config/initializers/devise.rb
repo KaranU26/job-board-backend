3dae3e9ad6f54d5407251bc5f376c24ab3a6b054
Karandeep Ubhi <karan_u26@hotmail.com>
2025-02-11 23:37:50 -0500

    Thought there was a secret key issue, but it seems fine

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -312,7 +312,7 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise_jwt_secret_key || ENV['DEVISE_JWT_SECRET_KEY']
+    jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
     jwt.dispatch_requests = [['POST', %r{^/api/v1/login$}]]
     jwt.revocation_requests = [['DELETE', %r{^/api/v1/logout$}]]
     jwt.expiration_time = 1.day.to_i
